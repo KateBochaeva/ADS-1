ef81059eb5381696ca0e29152febb59f553ca00a
KateBochaeva <117112623+KateBochaeva@users.noreply.github.com>
2023-02-25 21:26:06 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -6,12 +6,10 @@
 bool checkPrime(uint64_t value) {
   if (value == 0||value == 1)
     return false;
-  else
-  {
+  else {
     bool flag = true;
     for (int i = 2; i < value/2; i++)
-      if(value%i == 0)
-      {
+      if(value%i == 0) {
         flag = false;
         break;
       }
@@ -23,12 +21,10 @@ uint64_t nPrime(uint64_t n) {
     bool flag = true;
     int i = 1;
     int x = 0;
-    while (x != n)
-    {
+    while (x != n) {
       i++;
       for (int j = 2; j < i; j++)
-        if (i%j == 0)
-        {
+        if (i%j == 0) {
           flag = false;
           break;
         }
@@ -45,8 +41,7 @@ bool flag = false;
     flag = true;
     value++;
     for(int i = 2; i <= value/2; i++)
-      if (value%i == 0)
-      {
+      if (value%i == 0) {
         flag = false;
         break;
       }
@@ -57,11 +52,9 @@ bool flag = false;
 uint64_t sumPrime(uint64_t hbound) {
   uint64_t sum = 0;
   bool flag = true;
-  for(uint64_t i = 2; i < hbound; i++)
-  {
+  for(uint64_t i = 2; i < hbound; i++) {
     for(uint64_t j = 2; j <= i/2; j++)
-      if (i%j == 0)
-        {
+      if (i%j == 0) {
           flag = false;
           break;
         }

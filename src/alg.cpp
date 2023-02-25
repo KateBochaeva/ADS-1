52b11714333c0fc0ae6cbfc8a119b30e4a378e80
KateBochaeva <117112623+KateBochaeva@users.noreply.github.com>
2023-02-25 20:18:35 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -4,17 +4,70 @@
 
 
 bool checkPrime(uint64_t value) {
-// вставьте код функции
+  if (value==0||value==1)
+    return false;
+  else
+  {
+    bool flag=true;
+    for (int i=2; i<value/2; i++)
+      if(value%i==0)
+      {
+        flag=false;
+        break;
+      }
+      return flag;
+  }
 }
 
 uint64_t nPrime(uint64_t n) {
-// вставьте код функции
+    bool flag=true;
+    int i=1;
+    int x=0;
+    while (x!=n)
+    {
+      i++;
+      for (int j=2; j<i; j++)
+        if (i%j==0)
+        {
+          flag=false;
+          break;
+        }
+      if (flag==true)
+        x++;
+      flag=true;
+    }
 }
 
 uint64_t nextPrime(uint64_t value) {
-// вставьте код функции
+bool flag=false;
+  while(flag==false){
+    flag=true;
+    value++;
+    for(int i=2; i<=value/2; i++)
+      if (value%i==0)
+      {
+        flag=false;
+        break;
+      }
+  }
+  return value;
 }
 
 uint64_t sumPrime(uint64_t hbound) {
-// вставьте код функции
+  int sum=0;
+  bool flag=true;
+  for(int i=2; i<hbound; i++)
+  {
+    for(int j=2; j<=i/2; j++)
+      if (i%j==0)
+        {
+          flag=false;
+          break;
+        }
+    if (flag==true)
+      sum+=i;
+    else
+      flag=true;
+  }
+  return sum;
 }
